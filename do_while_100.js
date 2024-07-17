002f9b492c221376c3d66570c14614b3ea04367d
Edwin Habid Román Cardoso <edcarso020502@gmail.com>
2024-07-16 23:59:51 -0600

    Add files via upload

---
+++ do_while_100.js
@@ -0,0 +1,9 @@
+let suma =0;
+let num=1;
+
+do{
+    suma += num;
+    num++;
+    
+ console.log(suma)
+}while(num <=100)
---
+++ while_100.js
@@ -0,0 +1,9 @@
+let suma =0;
+let num=1;
+;
+while(num <=100){
+    suma += num;
+    num++;
+    
+ console.log(suma)
+}
